55eaa162511292498b6b931eeddaa8b02db78311
lilly <lilly1987@naver.com>
2023-03-05 22:27:06 +0900

    Update model-auto-change.py

--- scripts/model-auto-change.py
+++ scripts/model-auto-change.py
@@ -3,28 +3,38 @@ import gradio as gr
 import os
 import random
 
-from modules import images
+from modules import images, sd_models
 from modules.processing import process_images, Processed
 from modules.processing import Processed
 from modules.shared import opts, cmd_opts, state
 
-v_cnt=getattr(opts, f"model-auto-change-cnt", 0)
 
-def chg(enabled):
-    if not enabled:
-        logger.debug(f"{self.title()} disabled - exiting")
-        return p
-        
-    lst=sd_models.checkpoints_list.values()
-    print(f" opts.sd_model_checkpoint : { lst }")
-    #opts.sd_model_checkpoint 
-    print(f" opts.sd_model_checkpoint : { opts.sd_model_checkpoint }")
-    #sd_models.reload_model_weights()
-    print(f" cnt : { v_cnt }")
-    
-    return p
 
 class Script(scripts.Script):  
+    
+    v_cnt=getattr(opts, f"model-auto-change-cnt", 0)
+    v_model=None
+    
+    def chg(self, enabled):
+        if not enabled:
+            logger.debug(f"{self.title()} disabled - exiting")
+            return
+            
+        #global v_cnt
+        print(f" v_cnt : { self.v_cnt }")
+        if self.v_cnt<10:
+            self.v_cnt+=1
+            return
+        self.v_cnt=0
+        lst=list(sd_models.checkpoints_list.keys())
+        print(f" opts.sd_model_checkpoint : { lst }")
+        self.v_model=opts.sd_model_checkpoint 
+        opts.sd_model_checkpoint = random.choice(lst)
+        print(f" opts.sd_model_checkpoint : { opts.sd_model_checkpoint }")
+        sd_models.reload_model_weights()
+        print(f" cnt : { self.v_cnt }")
+        
+        return
     
     #def __init__(self):
     #    print(f"{self.title()} __init__")
@@ -51,10 +61,10 @@ class Script(scripts.Script):
 
     def ui(self, is_img2img):
         print(f"{self.title()} ui {is_img2img}")
-        enabled = gr.Checkbox(value=False, label="random apply")
-        with gr.Group():
-            with gr.Accordion(self.title(), open=False):
-                enabled = gr.Checkbox(value=False, label="random apply")
+        enabled = gr.Checkbox(value=False, label="model random apply")
+        #with gr.Group():
+        #    with gr.Accordion(self.title(), open=False):
+        #        enabled = gr.Checkbox(value=False, label="model random apply")
         return [enabled]
 
 
@@ -69,7 +79,7 @@ class Script(scripts.Script):
         , enabled
     ):  #noHypernetwork,rHypernetworks,sd_hypernetwork_strength1,sd_hypernetwork_strength2,
         print(f"{self.title()} process")
-        #chg(enabled)
+        self.chg(enabled)
         return 
         
         
@@ -86,5 +96,7 @@ class Script(scripts.Script):
     #    
     #    return chg(enabled)
     
-    #def postprocess(self, *args):
-    #    return
+    def postprocess(self, *args):
+        print(f"{self.title()} postprocess")
+        opts.sd_model_checkpoint = self.v_model
+        return
